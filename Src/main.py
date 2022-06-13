aec92491d2fb28bd4b4012c039a1965fb11cf33d
João Carlos <joaocarlos.losfe@gmail.com>
2022-06-13 19:03:42 -0300

    Update main.py

--- Src/main.py
+++ Src/main.py
@@ -1,7 +1,7 @@
 from gitclonous import *
 
 if __name__=="__main__":
-    repos = get_all_repositories("joaocarlos-losfe")
+    repos = get_all_repositories("github-username")
     
     for repo in repos:
         download_repository(repo, auto_extract=True)
